c56a6b73552a0bcb3583c0e1b0e53ac8d689c1a8
MdgSant <Miguelssantanatbl@gmail.com>
2026-02-24 15:19:35 -0300

    Projeto biscoito - data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('pw3-data-binding');
+  mensagem : any = [
+    "A",
+    "B",
+    "C",
+  ];
+
+  biscoito = "biscoito.png"
+  fechado: boolean = true;
+
+  abrir(){
+    if(this.fechado == true){
+      this.fechado = false; 
+      this.biscoito = "quebrado.png";
+    }
+  }
+
+  fechar(){
+    if(!this.fechado){
+      this.fechado = true; 
+      this.biscoito = "biscoito.png";
+    }
+  }
+
+  
+
 }
